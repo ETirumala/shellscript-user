879b7bad30b7b57a6fc426fde4c139d9b4482f65
ETirumala <45357298+ETirumala@users.noreply.github.com>
2019-04-28 00:33:59 +0530

    Update userlist.sh

--- userlist.sh
+++ userlist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #Creating a User and password
-user_name=sathyapriya
-useradd ${user_name} -p sp.sathya@14
+user_name=spriya
+useradd ${user_name} -p sp.s@14
 echo ${user_name} Created Successfully.
